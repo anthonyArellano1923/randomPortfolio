404b9f1debfa271dd35b7eeba4bafdddba6c29f1
anthonyArellano1923 <163557083+anthonyArellano1923@users.noreply.github.com>
2025-08-01 10:22:17 -0400

    Add initial project structure with images, styles, and templates
    - Added images for GitHub, Instagram, and Twitter.
    - Created main CSS and Stylus files for styling.
    - Implemented a template to display user data fetched from an API.
    - Set up a utility function to fetch data from the API.
    - Configured Webpack for development and production environments, including asset management for images and fonts.
    - Established a basic HTML structure with a template for rendering user information.

---
+++ webpack.config.dev.js
@@ -0,0 +1,73 @@
+
+//Importando 'path' para un proyecto type: "module"
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+//Plugins
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import Dotenv from 'dotenv-webpack' 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+//--------------------------------------------------
+
+export default { //En clase, esta linea es 'module.export =', pero como estoy trabajando con modulos, es necesario cambiarla.
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name][contenthash].js",
+  },
+  mode: "development",
+  watch: true,
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@templates": path.resolve(__dirname, "src/templates"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+      "@images": path.resolve(__dirname, "src/assets/images"),
+    } 
+  },
+  module: {
+    rules: [
+        {//Manejador de lógica .js
+          test: /\.m?js$/, //Tipo de extensiones con el que se trabajará para importar javascript. 
+          exclude: /node_modules/, //Esto evita que se utilicen los archivos que estén dentro de la carpeta node_modules, como los .js, que podrían romper la lógica de la aplicación.
+          use: "babel-loader"
+        },
+        {//Manejador de estilos
+          test: /\.css|styl$/i,//Expresión regular que indica la extensión de los archivos con los que se trabajarán los estilos.
+          use: [MiniCssExtractPlugin.loader,
+            'css-loader',
+            'stylus-loader'
+          ]
+        }, 
+        {// Manejador de imágenes
+          test: /\.png/,
+          type: "asset/resource",
+          generator: {
+            filename: "assets/images/[hash][ext][query]"
+          }
+        },
+        {//Manejador de fuentes
+          test: /\.(woff|woff2)$/i,
+          type: "asset/resource",
+          generator: {
+            filename: "assets/fonts/[hash][ext][query]"
+          }
+        }
+      ]
+    },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './public/index.html', //Buscael html principal del proyecto.
+      filename: './index.html' //Este es el nombre y la ubicación del html optimizado por webpack.
+      }),
+    new MiniCssExtractPlugin({
+      filename: "assets/[name].[contenthash].css"
+    }),
+    new Dotenv()
+    ]
+}
